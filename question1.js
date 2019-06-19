adb39e0a706de1cb2085c8d7c9e6cd3083a63661
liqiudeng <51063801+liqiudeng@users.noreply.github.com>
2019-06-19 16:35:51 -0400

    first question

--- question1.js
+++ question1.js
@@ -1,8 +1,10 @@
-function sumpositive(arr1){
-    var sum;
-    for (var i=0;i<arr1.length; i++)
-    if (arr1[i]>0)
-    sum+= arr1[i];
+let sumpositive = arr => {
+  let sum = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+
+  return sum;
 };
-var arr1=[1,-4,7,12];
-console.log(sumpositive(arr1));
